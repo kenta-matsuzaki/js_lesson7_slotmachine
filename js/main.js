92245cc7a5b644e9721b092853e7f83a5d8caaad
kenta-matsuzaki <kenta6910@gmail.com>
2019-08-26 20:41:58 +0900

    Summary(required)

--- js/main.js
+++ js/main.js
@@ -7,11 +7,23 @@
       section.classList.add('panel');
 
       this.img = document.createElement('img');
-      this.img.src = 'img/seven.png';
+      this.img.src = 'img/pic00.png';
 
       this.stop = document.createElement('div');
       this.stop.textContent = 'STOP';
       this.stop.classList.add('stop');
+
+      section.appendChild(this.img);
+      section.appendChild(this.stop);
+
+      const main = document.querySelector('main');
+      main.appendChild(section);
     }
   }
+  
+  const panels = [
+    new Panel(),
+    new Panel(),
+    new Panel(),
+  ];
 }
